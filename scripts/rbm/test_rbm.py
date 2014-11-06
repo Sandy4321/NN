c2ec656348849a22efc131dd20f9760389747bed
Daniel <deworrall92@gmail.com>
2014-11-06 22:23:49 +0000

    Implemented unit test with nose, using Chebyshev's ineq

--- scripts/rbm/test_rbm.py
+++ scripts/rbm/test_rbm.py
@@ -4,32 +4,92 @@
 #
 # Set of unit tests for the rbm extention script
 
-import numpy as np
-import rbm_header as rbm
-
 # Test 1
 # Do we generate samples from a bernoulli distribution roughly?
 
-
+import numpy as np
+import rbm_header as rbm
 print "Testing vector sampler"
-n = 100000.0
-m = [0.0, 0.1, 0.25,  0.5, 0.75, 1.0]
-print "Mean:", m[0], "empirical mean:", np.average(rbm.bern_samp(m[0] * np.ones((n)),n))
-print "Mean:", m[1], "empirical mean:", np.average(rbm.bern_samp(m[1] * np.ones((n)),n))
-print "Mean:", m[2], "empirical mean:", np.average(rbm.bern_samp(m[2] * np.ones((n)),n))
-print "Mean:", m[3], "empirical mean:", np.average(rbm.bern_samp(m[3] * np.ones((n)),n))
-print "Mean:", m[4], "empirical mean:", np.average(rbm.bern_samp(m[4] * np.ones((n)),n))
-print "Mean:", m[5], "empirical mean:", np.average(rbm.bern_samp(m[5] * np.ones((n)),n))
 
-# Test 2
-print "Testing matrix sampler"
-R = 1000
-K = 1000
-m = [0.0, 0.1, 0.25,  0.5, 0.75, 1.0]
-print "Mean:", m[0], "empirical mean:", np.average(rbm.bern_samp_mat(m[0] * np.ones((R,K)),(R,K)))
-print "Mean:", m[1], "empirical mean:", np.average(rbm.bern_samp_mat(m[1] * np.ones((R,K)),(R,K)))
-print "Mean:", m[2], "empirical mean:", np.average(rbm.bern_samp_mat(m[2] * np.ones((R,K)),(R,K)))
-print "Mean:", m[3], "empirical mean:", np.average(rbm.bern_samp_mat(m[3] * np.ones((R,K)),(R,K)))
-print "Mean:", m[4], "empirical mean:", np.average(rbm.bern_samp_mat(m[4] * np.ones((R,K)),(R,K)))
-print "Mean:", m[5], "empirical mean:", np.average(rbm.bern_samp_mat(m[5] * np.ones((R,K)),(R,K)))
+def test_rbm_smpl():
+    '''Test vector sampler'''
+    
+    import numpy as np
+    import rbm_header as rbm
+    
+    n = 10000.0
+    K = np.linspace(1,2,10)
+    M = [0.0, 0.1, 0.25,  0.5, 0.75, 1.0]
+    
+    fails = np.zeros((60))
+    
+    for i,m in enumerate(M):
+        s = np.sqrt(m*(1-m))
+        arg = np.abs(rbm.bern_samp(m * np.ones((n,1)),(n,1)) - m)
+        for j,k in enumerate(K):
+            P = sum(arg > k*s)/n
+            if (P > (1/k**2)):
+                fails[i*10 + j] = True
+    
+    print "Failed at ", [i for i,j in enumerate(fails) if j == True]
+    assert sum(fails) == False
+
+
+def test_rbm_smpl_mat():
+    '''Test matrix sampler'''
+    
+    import numpy as np
+    import rbm_header as rbm
+    
+    h = 1000
+    w = 1000
+    K = np.linspace(1,2,10)
+    M = [0.0, 0.1, 0.25,  0.5, 0.75, 1.0]
+    
+    fails = np.zeros((60))
+    
+    for i,m in enumerate(M):
+        s = np.sqrt(m*(1-m))
+        arg = np.abs(rbm.bern_samp_mat(m * np.ones((h,w)),(h,w)) - m)
+        for j,k in enumerate(K):
+            P = sum(sum(arg > k*s))/(h*w)
+            if (P > (1/k**2)):
+                fails[i*10 + j] = True
+    
+    print "Failed at ", [i for i,j in enumerate(fails) if j == True]
+    assert sum(fails) == False
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
