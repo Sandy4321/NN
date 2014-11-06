ee6a5a9a3acb49b6ca65c3bc1012b06b99a7970d
Daniel <d.worrall@cs.ucl.ac.uk>
2014-11-06 21:32:03 +0000

    Updating unit tests

--- scripts/rbm/test_rbm.py
+++ scripts/rbm/test_rbm.py
@@ -1,4 +1,4 @@
-# rbm_unit.py
+# test_rbm.py
 #
 # Daniel E. Worrall --- 27 Oct 2014
 #
@@ -9,15 +9,17 @@ import rbm_header as rbm
 
 # Test 1
 # Do we generate samples from a bernoulli distribution roughly?
+
+
 print "Testing vector sampler"
 n = 1000000.0
 m = [0.0, 0.1, 0.25,  0.5, 0.75, 1.0]
-print "Mean:", m[0], "empirical mean:", np.average(rbm.bern_samp(m[0] * np.ones((n,1)),n))
-print "Mean:", m[1], "empirical mean:", np.average(rbm.bern_samp(m[1] * np.ones((n,1)),n))
-print "Mean:", m[2], "empirical mean:", np.average(rbm.bern_samp(m[2] * np.ones((n,1)),n))
-print "Mean:", m[3], "empirical mean:", np.average(rbm.bern_samp(m[3] * np.ones((n,1)),n))
-print "Mean:", m[4], "empirical mean:", np.average(rbm.bern_samp(m[4] * np.ones((n,1)),n))
-print "Mean:", m[5], "empirical mean:", np.average(rbm.bern_samp(m[5] * np.ones((n,1)),n))
+print "Mean:", m[0], "empirical mean:", np.average(rbm.bern_samp(m[0] * np.ones((n)),n))
+print "Mean:", m[1], "empirical mean:", np.average(rbm.bern_samp(m[1] * np.ones((n)),n))
+print "Mean:", m[2], "empirical mean:", np.average(rbm.bern_samp(m[2] * np.ones((n)),n))
+print "Mean:", m[3], "empirical mean:", np.average(rbm.bern_samp(m[3] * np.ones((n)),n))
+print "Mean:", m[4], "empirical mean:", np.average(rbm.bern_samp(m[4] * np.ones((n)),n))
+print "Mean:", m[5], "empirical mean:", np.average(rbm.bern_samp(m[5] * np.ones((n)),n))
 
 # Test 2
 print "Testing matrix sampler"
