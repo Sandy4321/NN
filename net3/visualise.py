f573329b4e8e635f519169ac28445dfebd44237e
Daniel <deworrall92@gmail.com>
2015-05-15 23:03:25 +0100

    vis

--- net3/visualise.py
+++ net3/visualise.py
@@ -8,55 +8,57 @@ import utils
 from matplotlib import pyplot as plt
 from PIL import Image
 
-fname = './pkl/drop0.pkl'
-stream = open(fname, 'r')
-state = cPickle.load(stream)
-stream.close()
-
-monitor = state['monitor']
-args = state['hyperparams']
-print('Validation cost %f' % (monitor['best_cost'],))
-
-params, hypparams = monitor['best_model']
-
-
-for param in params:
-    print param
-    print('Max: %f' % (numpy.amax(param.get_value()),))
-    print('Min: %f' % (numpy.amin(param.get_value()),))
-    pylab.figure()
-    pylab.hist(param.get_value().flatten(), 50, normed=1)
-    pylab.suptitle(param, fontsize=20)
-    pylab.show()
-
-
-
-fig = plt.figure()
-plt.semilogx(monitor['valid_cost'],'b')
-plt.show()
-
-fig = plt.figure()
-plt.semilogx(monitor['train_cost'],'r')
-plt.show()
-for arg in args:
-    if arg != 'dropout_dict':
-        print arg, args[arg]
-
-n = 20
-for param in params:
-    W = param.get_value()
-    Wsh = W.shape
-    if Wsh[1] > 1:
-        idx = numpy.random.choice(Wsh[0], n**2, replace=False)
-        print('Max: %f' % (numpy.amax(W),))
-        print('Min: %f' % (numpy.amin(W),))
-        w = W[idx,:]
-        im = Image.fromarray(utils.tile_raster_images(X=w,
-                                                      img_shape=(28,28),
-                                                      tile_shape=(n,n),
-                                                      tile_spacing=(1,1)))
-        im.show()
-     
+for i in numpy.arange(5):
+    fname = './pkl/drop' + str(i) + '.pkl'
+    stream = open(fname, 'r')
+    state = cPickle.load(stream)
+    stream.close()
+    
+    monitor = state['monitor']
+    args = state['hyperparams']
+    print('Validation cost %f' % (monitor['best_cost'],))
+    
+    params, hypparams = monitor['best_model']
+    
+    
+    for param in params:
+        print param
+        print('Max: %f' % (numpy.amax(param.get_value()),))
+        print('Min: %f' % (numpy.amin(param.get_value()),))
+        pylab.figure()
+        pylab.hist(param.get_value().flatten(), 50, normed=1)
+        pylab.suptitle(param, fontsize=20)
+        pylab.show()
+    
+    
+    
+    fig = plt.figure()
+    plt.semilogx(monitor['valid_cost'],'b')
+    plt.show()
+    
+    fig = plt.figure()
+    plt.semilogx(monitor['train_cost'],'r')
+    plt.show()
+    for arg in args:
+        if arg != 'dropout_dict':
+            print arg, args[arg]
+    
+    """
+    n = 20
+    for param in params:
+        W = param.get_value()
+        Wsh = W.shape
+        if Wsh[1] > 1:
+            idx = numpy.random.choice(Wsh[0], n**2, replace=False)
+            print('Max: %f' % (numpy.amax(W),))
+            print('Min: %f' % (numpy.amin(W),))
+            w = W[idx,:]
+            im = Image.fromarray(utils.tile_raster_images(X=w,
+                                                          img_shape=(28,28),
+                                                          tile_shape=(n,n),
+                                                          tile_spacing=(1,1)))
+            im.show()
+    """
         
         
         
