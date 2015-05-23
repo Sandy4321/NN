6ad893c1cce75b2d5e616c4a081c6f433af2c2bb
Daniel <deworrall92@gmail.com>
2015-05-23 17:08:20 +0100

    dropconnect

--- net3/mlp.py
+++ net3/mlp.py
@@ -41,7 +41,7 @@ class Mlp():
             #W_value = 2*coeff*(numpy.random.uniform(size=(self.ls[i+1],
                                                           #self.ls[i]))-0.5)
             coeff = numpy.sqrt(0.1)
-            W_value = coeff*numpy.random.randn((self.ls[i+1],self.ls[i]))
+            W_value = coeff*numpy.random.randn(self.ls[i+1],self.ls[i])
             W_value = numpy.asarray(W_value, dtype=Tconf.floatX)
             Wname = 'W' + str(i)
             self.W.append(TsharedX(W_value, Wname, borrow=True))
