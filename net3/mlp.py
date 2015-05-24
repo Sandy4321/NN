390df3b4c1c8a892e136533b56c8a593a47f2892
Daniel <deworrall92@gmail.com>
2015-05-24 13:09:00 +0100

    dropconnect

--- net3/mlp.py
+++ net3/mlp.py
@@ -94,13 +94,11 @@ class Mlp():
                 G = self.dropconnect(layer, size)
                 #self.G.append(G > 0)        # To access mask values
                 print X.broadcastable
-                print G.broadcastable
                 W = self.W[layer].dimshuffle(0,1,'x')
-                print W.broadcastable
                 H = W*G
                 print H.broadcastable
-                pre_act = T.tensordot(H,X,axes=[[1],[0]]) + self.b[layer]
-                print self.b[layer].broadcastable
+                pre_act = T.tensordot(H,X,axes=[[1],[0]])
+                pre_act = pre_act.diagonal(axis1=1,axis2=2) + self.b[layer]
                 print pre_act.broadcastable
         else:
             print('Non-drop layer')
