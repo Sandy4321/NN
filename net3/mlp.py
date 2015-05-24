19e85a5995bc4b4b969822d63f69da63fd44d9d1
Daniel <deworrall92@gmail.com>
2015-05-24 23:43:15 +0100

    sparsity

--- net3/mlp.py
+++ net3/mlp.py
@@ -40,6 +40,8 @@ class Mlp():
         for i in numpy.arange(self.num_layers):
             if 'connectivity' in args:
                 beta = args['connectivity'][i]
+            else:
+                beta = 1.
             #Connection weights
             coeff = numpy.sqrt(6/(beta*(self.ls[i] + self.ls[i+1])))
             W_value = 2*coeff*(numpy.random.uniform(size=(self.ls[i+1],
