1d6960814805c1e726a33ee5724efa51cf40ff15
Daniel <deworrall92@gmail.com>
2015-05-25 20:43:06 +0100

    init

--- net3/DGWN.py
+++ net3/DGWN.py
@@ -44,7 +44,7 @@ class Dgwn():
                                    broadcastable=(False,True)))
             # Connection weight means
             coeff = 0.001*numpy.sqrt(2/(self.ls[i] + self.ls[i+1]))
-            M_value = coeff*(numpy.random.randn(self.ls[i+1],self.ls[i]))
+            M_value = coeff*(numpy.random.randn(self.ls[i+1],self.ls[i]))-1.
             M_value = numpy.asarray(M_value, dtype=Tconf.floatX)
             Mname = 'M' + str(i)
             self.M.append(TsharedX(M_value, Mname, borrow=True))
