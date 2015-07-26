09067b60ff659701daf4177103c9ec15b7276fa4
Daniel <deworrall92@gmail.com>
2015-07-26 12:09:43 +0100

    .

--- net3/prune.py
+++ net3/prune.py
@@ -1,28 +1,15 @@
 '''Pruning'''
 
-__authors__   = "Daniel Worrall"
-__copyright__ = "(c) 2015, University College London"
-__license__   = "3-clause BSD License"
-__contact__   = "d.worrall@cs.ucl.ac.uk"
-
 import os, time, sys
 
 import cPickle
 import numpy
 
-from convnet import Convnet
+from mlp import Mlp
 from theano import config as Tconf
 from train import DivergenceError, Train
 
-def objective(args):
-    lr, mmtm = args
-    for arg in args:
-        print arg
-    layers = {}
-    layers['input'] = { 'name':'input', 'type':'data', 'shape':(128,1,28,28)}
-    layers['fc1'] = { 'name':'fc1', 'input':'input', 'type':'fc', 'shape':(800,), 'nonlin':'ReLU', 'dropout' : 0.8, 'max_norm' : numpy.sqrt(15.)}
-    layers['fc2'] = { 'name':'fc2', 'input':'fc1', 'type':'fc', 'shape':(800,), 'nonlin':'ReLU', 'dropout' : 0.5, 'max_norm' : numpy.sqrt(15.)}
-    layers['output'] = { 'name':'output', 'input':'fc2', 'type':'fc', 'shape':(10,), 'nonlin':'SoftMax', 'dropout' : 0.5, 'max_norm' : numpy.sqrt(15.)}
+def objective(lr):
     args = {
         'algorithm' : 'SGD',
         'RMScoeff' : None,
@@ -30,45 +17,67 @@ def objective(args):
         'mode' : 'training',
         'learning_type' : 'classification',
         'num_classes' : 10,
-        'train_cost_type' : 'nll',
+        'train_cost_type' : 'cross_entropy',
         'valid_cost_type' : 'accuracy',
-        'layers' : layers,
+        'layer_sizes' : (784, 800, 800, 10),
+        'nonlinearities' : ('ReLU', 'ReLU', 'SoftMax'),
         'data_address' : './data/mnist.pkl.gz',
         'binarize': False,
-        'zero_mean' : False,
         'learning_rate' : lr,
-        'lr_multipliers' : {'b' : 2},
+        'lr_multipliers' : {'b' : 2.},
         'learning_rate_margin' : (0,200,300),
         'learning_rate_schedule' : ((1.,),(0.5,0.1),(0.05,0.01,0.005,0.001)),
-        'momentum' : mmtm,
+        'momentum' : 0.9,
         'momentum_ramp' : 0,
+        'batch_size' : 128,
+        'num_epochs' : 500,
+        'prior' : 'Gaussian',
+        'prior_variance' : None,
+        'num_components' : 1,
+        'num_samples' : 1,
         'norm' : 'L2',
         'max_row_norm' : numpy.sqrt(15.),
-        'batch_size' : layers['input']['shape'][0],
-        'num_epochs' : 500,
+        'zero_mean' : False,
+        'sparsity' : None, 
+        'dropout_dict' : None,
         'cov' : False,
         'validation_freq' : 10,
-        'save_freq' : 10,
-        'save_name' : 'pkl/preprunedmlp.pkl'
+        'save_freq' : 50,
+        'save_name' : 'pkl/MLP.pkl'
         }
-     
+
+    dropout_dict = {}
+    for i in numpy.arange(len(args['nonlinearities'])):
+        name = 'layer' + str(i)
+        size = (args['layer_sizes'][i],1)
+        if i == 0:
+            # Need to cast to floatX or the computation gets pushed to the CPU
+            prior = 0.8*numpy.ones(size).astype(Tconf.floatX)
+        else:
+            prior = 0.5*numpy.ones(size).astype(Tconf.floatX)
+        sub_dict = { name : {'seed' : 234,
+                             'type' : 'unbiased',
+                             'values' : prior}}
+        dropout_dict.update(sub_dict)
+    args['dropout_dict'] = dropout_dict
+        
     tr = Train()
-    tr.construct(Convnet, args)
+    tr.construct(Mlp, args)
     tr.build(args)
     tr.load_data(args)
     monitor = tr.train(args)
-    print(monitor['best_cost'])
-        
 
-if __name__ == '__main__':
-    lrs = [1e1, 3e0, 1e0, 3e-1, 1e-1, 3e-2, 1e-2, 3e-3, 1e-3, 3e-4, 1e-4, 3e-5, 1e-5]
-    for lr in lrs:
-        print('Learning rate: %f' % lr)
-        try:
-            objective((lr, 0.9))
-        except DivergenceError:
-            pass
-        
+
+lrs = [3e-2, 1e-2, 3e-3, 1e-3, 3e-4, 1e-4, 3e-5, 1e-5]
+for lr in lrs:
+    print('Learning rate: %f' % lr)
+    try:
+        objective(lr)
+    except DivergenceError:
+        pass
+
+
+
 
 
 
