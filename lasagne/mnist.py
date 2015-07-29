7f14a6b2c23684466a1e642323e09c2f937416ce
Daniel <deworrall92@gmail.com>
2015-07-29 23:16:31 +0100

    DWGN

--- lasagne/mnist.py
+++ lasagne/mnist.py
@@ -233,7 +233,8 @@ class GaussianLayer(lasagne.layers.Layer):
         self.nonlinearity = nonlinearity
 
     def get_output_for(self, input, **kwargs):
-        b = T.ones_like(input[:,0])
+        b = T.ones_like(input[:,0]).dimshuffle(0,'x')
+        print b.broadcastable
         X = T.concatenate([input,b],axis=1)
         M = T.dot(X,self.M) 
         S = T.sqrt(T.dot(X**2,T.log(1 + T.exp(self.R))**2))
