20c1af909c988629c3760d7571d8ad19c9881d86
Daniel <deworrall92@gmail.com>
2015-08-01 12:43:06 +0100

    .

--- lasagne/BNN.py
+++ lasagne/BNN.py
@@ -143,7 +143,7 @@ def main(model='mlp', num_epochs=500):
     loss = loss.mean()
     # We could add some weight decay as well here, see lasagne.regularization.
     for layer in lasagne.layers.get_all_layers(network):
-        if hasattr(layer, layer_type):
+        if hasattr(layer, 'layer_type'):
             print layer.layer_type
     # Create update expressions for training, i.e., how to modify the
     # parameters at each training step. Here, we'll use Stochastic Gradient
