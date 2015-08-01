05855f4cbf478d2cfb9af20ff4fddca2c2b48ffc
Daniel <deworrall92@gmail.com>
2015-08-01 16:36:09 +0100

    .

--- lasagne/BNN.py
+++ lasagne/BNN.py
@@ -75,13 +75,13 @@ def load_dataset():
 def build_mlp(input_var=None):
     l_in = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
                                      input_var=input_var)
-    l_hid1 = HalfGaussianLayer(
+    l_hid1 = GaussianLayer(
             l_in, num_units=800,
             nonlinearity=lasagne.nonlinearities.rectify)
-    l_hid2 = HalfGaussianLayer(
+    l_hid2 = GaussianLayer(
             l_hid1, num_units=800,
             nonlinearity=lasagne.nonlinearities.rectify)
-    l_out = HalfGaussianLayer(
+    l_out = GaussianLayer(
             l_hid2, num_units=10,
             nonlinearity=lasagne.nonlinearities.softmax)
     return l_out
@@ -258,7 +258,7 @@ class GaussianLayer(lasagne.layers.Layer):
         s = T.sqrt(T.dot(X**2,self.S**2))
         smrg = MRG_RandomStreams()
         E = smrg.normal(size=s.shape)
-        H = M + s*E 
+        H = M + M*s*E 
         # Nonlinearity
         return self.nonlinearity(H)
 
