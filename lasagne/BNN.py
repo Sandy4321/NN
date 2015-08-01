cdb3be3ab6db4a9a7c520554e7fb8e576fe5bab7
Daniel <deworrall92@gmail.com>
2015-08-01 12:51:12 +0100

    .

--- lasagne/BNN.py
+++ lasagne/BNN.py
@@ -146,7 +146,7 @@ def main(model='mlp', num_epochs=500):
     for layer in lasagne.layers.get_all_layers(network):
         if hasattr(layer, 'layer_type'):
             if layer.layer_type == 'GaussianLayer':
-                loss += GaussianRegulariser(layer.M, layer.R, 0.001)/dataset_size
+                loss += GaussianRegulariser(layer.M, layer.R, 0.01)/dataset_size
     # Create update expressions for training, i.e., how to modify the
     # parameters at each training step. Here, we'll use Stochastic Gradient
     # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
