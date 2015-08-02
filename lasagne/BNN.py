a22754a83410bee2ff9da151d198d9520dce6965
Daniel <deworrall92@gmail.com>
2015-08-02 19:35:16 +0100

    model saving

--- lasagne/BNN.py
+++ lasagne/BNN.py
@@ -74,15 +74,15 @@ def load_dataset():
 
 def build_mlp(input_var=None):
     l_in = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
-                                     input_var=input_var)
+                                     input_var=input_var, name='l_in')
     l_hid1 = FullGaussianLayer(
-            l_in, num_units=800,
+            l_in, num_units=800, name='l_hid1',
             nonlinearity=lasagne.nonlinearities.rectify)
     l_hid2 = FullGaussianLayer(
-            l_hid1, num_units=800,
+            l_hid1, num_units=800, name='l_hid2',
             nonlinearity=lasagne.nonlinearities.rectify)
     l_out = FullGaussianLayer(
-            l_hid2, num_units=10,
+            l_hid2, num_units=10, name='l_out',
             nonlinearity=lasagne.nonlinearities.softmax)
     return l_out
 
