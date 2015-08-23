a9a15b9522a7b24cde9dcf251ed16a5a6b0cd08d
Daniel <deworrall92@gmail.com>
2015-08-23 18:00:34 +0100

    .

--- lasagne/BNN.py
+++ lasagne/BNN.py
@@ -359,7 +359,7 @@ def main(model='mlp', num_epochs=100, file_name=None, proportion=0.,
     learning_rate = T.fscalar('learning_rate')
     #updates = nesterov_momentum(loss, params, learning_rate=learning_rate,
     #                            momentum=0.9)
-    updates = lasange.updates.rmsprop(loss, params, learning_rate=learning_rate)
+    updates = lasagne.updates.rmsprop(loss, params, learning_rate=learning_rate)
     # Create a loss expression for validation/testing. The crucial difference
     # here is that we do a deterministic forward pass through the network,
     # disabling dropout layers.
