7caf12808380a1fb35f50e248b4cfd4fd2d16b0e
Daniel <deworrall92@gmail.com>
2015-08-30 18:51:26 +0100

    vppd - SGLD second attempt

--- lasagne/VPPD.py
+++ lasagne/VPPD.py
@@ -193,6 +193,7 @@ def main(model='mlp', num_epochs=100, file_name=None, proportion=0.,
     log_prior = 0.
     for param in params:
         if param == 'W':
+            print('visited')
             value = param.get_value(borrow=True)
             log_prior += -0.5*1.*T.sum(value**2) # Need to add layer wise
     
