02450b5dc3d4f9db96234c8fa2159c00f05ef980
Daniel <deworrall92@gmail.com>
2015-08-30 18:53:52 +0100

    vppd - SGLD second attempt

--- lasagne/VPPD.py
+++ lasagne/VPPD.py
@@ -192,6 +192,7 @@ def main(model='mlp', num_epochs=100, file_name=None, proportion=0.,
     # The prior
     log_prior = 0.
     for param in params:
+        print('not visited')
         if param.name == 'W':
             print('visited')
             value = param.get_value(borrow=True)
